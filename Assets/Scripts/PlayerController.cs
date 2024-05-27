da3a5049769894a8efb133e65aa413b26ac44049
Brandon Humboldt <brhumboldt@gmail.com>
2024-05-26 20:02:58 -0500

    player animation and flipping on x axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,12 @@ public class PlayerController : MonoBehaviour
     private PlayerControls _playerControls;
     private Rigidbody _rigidbody;
     private Vector3 _movement;
-    
+
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _sprite;
     [SerializeField] private int speed;
-    
+
+    private const string IS_WALK_PARAM = "IsWalk";
     
     void Awake()
     {
@@ -35,6 +38,18 @@ public class PlayerController : MonoBehaviour
         float moveZ = move.y;
         
         _movement = new Vector3(moveX, 0, moveZ).normalized;
+        
+        _animator.SetBool(IS_WALK_PARAM, _movement != Vector3.zero);
+
+        if (moveX != 0 && moveX < 0)
+        {
+            _sprite.flipX = true;
+        }
+
+        if (moveX != 0 && moveX > 0)
+        {
+            _sprite.flipX = false;
+        }
     }
     
     private void FixedUpdate()
